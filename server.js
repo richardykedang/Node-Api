323f5248b9025a6160d1ca2f80c770a47eee1eab
richardykedang <kedangrichard@gmail.com>
2020-12-04 03:04:25 +0700

    Setting route with json object

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+// ROUTE
+app.get('/', (req, res) => {
+    res
+        .status(200)
+        .json(
+                {
+                "name":"John",
+                "age":30,
+                "cars":[ "Ford", "BMW", "Fiat" ]
+                }
+            );
+})
 const PORT = 3000
 app.listen(3000, ()=>{
     console.log(`App running on port : ${PORT}`)
