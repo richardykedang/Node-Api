3e9d36ed669e7c1851c2a15e76e3bf452aa8f13c
richardykedang <kedangrichard@gmail.com>
2020-12-04 03:48:35 +0700

    Get Request

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
+const { json } = require('express');
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
+//READ FILE
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+//console.log(tours)
+
 // ROUTE
-app.get('/', (req, res) => {
-    res
-        .status(200)
-        .json(
-                {
-                "name":"John",
-                "age":30,
-                "cars":[ "Ford", "BMW", "Fiat" ]
-                }
-            );
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        message : 'success',
+        result : tours.length,
+        data : {
+            tours
+        }
+    }) 
 })
 const PORT = 3000
 app.listen(3000, ()=>{
