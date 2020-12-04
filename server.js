c5df63f69a9115401e169acf30f87915fd1d12ba
richardykedang <kedangrichard@gmail.com>
2020-12-04 09:39:40 +0700

    handling post request

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json());
 
 //READ FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 //console.log(tours)
 
 // ROUTE
+//GET ALL DATA
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         message : 'success',
@@ -17,7 +19,29 @@ app.get('/api/v1/tours', (req, res) => {
             tours
         }
     }) 
+});
+
+//POST
+//CREATE NEW ONE
+app.post('/api/v1/tours', (req,res) => {
+    //console.log(req.body);
+    newId = tours[tours.length - 1].id + 1;
+    //console.log(newId);
+    newTour = Object.assign({id : newId}, req.body);
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), (err) => {
+        res.status(200).json({
+            message : 'success',
+            data : {
+                tours : newTour
+            }
+        })
+    })
+    //res.send("Done");
 })
+
+
 const PORT = 3000
 app.listen(3000, ()=>{
     console.log(`App running on port : ${PORT}`)
