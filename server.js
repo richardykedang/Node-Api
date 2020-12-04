649d4ba92441d40c7aabae157af387520f435c6b
richardykedang <kedangrichard@gmail.com>
2020-12-04 10:32:32 +0700

    get the single one request

--- server.js
+++ server.js
@@ -21,6 +21,26 @@ app.get('/api/v1/tours', (req, res) => {
     }) 
 });
 
+//GET SINGLE ONE
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+    //console.log(tour)
+    if(!tour){
+        return res.status(404).json({
+            status : 'fail',
+            data : 'INVALID ID'
+        })
+    }
+    res.status(200).json({
+        status : 'success',
+        data : {
+            tour
+        }
+    }) 
+})
+
 //POST
 //CREATE NEW ONE
 app.post('/api/v1/tours', (req,res) => {
